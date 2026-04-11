5222c9e8d90e5b9b6add3fe419b68e2c9bcb5bab
Antariksha <kumarantar98@gmail.com>
2026-04-11 09:06:19 +0700

    security: escape HTML entities before markdown rendering to prevent XSS

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -15,10 +15,22 @@ const formattedTime = computed(() => {
 })
 
 /**
+ * Escape HTML entities to prevent XSS
+ */
+function escapeHtml(str: string): string {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+/**
  * Minimal markdown: **bold** and *italic*
  */
 function renderMarkdown(text: string): string {
-  return text
+  return escapeHtml(text)
     .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
     .replace(/\*(.+?)\*/g, '<em>$1</em>')
     .replace(/\n/g, '<br>')
